ea759555524aa680ad0d21cce2b8691f6098b930
juanc <juanc@Juan_Carlos>
2024-03-12 12:09:20 -0600

    Cra un programa en java que dependido de que radio button escoja nos arojar en mensaje en patalla que diga si es hombre o mujer

---
+++ src/sexos/Sexos.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sexos;
+
+/**
+ *
+ * @author juanc
+ */
+public class Sexos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Sexos1 SEX = new Sexos1();
+        SEX.setVisible(true);
+        SEX.setLocationRelativeTo(null);
+    }
+    
+}
